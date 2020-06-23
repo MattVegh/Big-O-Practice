f5ee03cb7ec961e44271edacaa27310862772554
Matt Vegh <mdv240@gmail.com>
2020-06-23 15:34:06 -0500

    exponential

--- BigO.js
+++ BigO.js
@@ -118,3 +118,31 @@ function hasDuplicates(array) {
 console.log(hasDuplicates([1, 2, 3]))
 console.log(hasDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 9]))
 console.log(hasDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 99]))
+
+// Exponential Time O(2^n) example
+
+function countTriangle(layers) {
+    let ticks = 1;
+    let count = 0; // the number of dots we've counted so far
+    let layer = 0; // the current layer we're on
+    let lastLayer = 1; // the number of dots we counted in the previous layer
+    while (layer < layers) {
+        ticks++;
+        let newLayer = 0; // the number of dots we've counted so far in the current layer
+        for (let i = 0; i < lastLayer; i++) {
+            ticks++;
+            newLayer += 2;
+        }
+        lastLayer = newLayer;
+        count += lastLayer;
+        layer++;
+    }
+    return {
+        result: count,
+        ticks: ticks
+    };
+}
+
+console.log(countTriangle(2))
+console.log(countTriangle(4))
+console.log(countTriangle(16))
